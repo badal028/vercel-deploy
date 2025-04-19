b53a1b442414dc171475f74697082a0407ed525b
badal028 <prajapati.badal@cloudesign.com>
2025-04-19 19:43:56 +0530

    fixess

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['@vercel/analytics/react', 'framer-motion', 'react-helmet-async'],
-      output: {
-        globals: {
-          '@vercel/analytics/react': 'VercelAnalytics',
-          'framer-motion': 'FramerMotion',
-          'react-helmet-async': 'ReactHelmetAsync'
-        }
-      }
-    }
+    sourcemap: true
   }
 })
